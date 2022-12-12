e910b38c30d0db486c255630e3e583b2dc9b6687
Oleg Gaivoronskiy <o.gajvoronskij03@gmail.com>
2022-12-12 17:07:20 +0300

    my defence

--- myjs.js
+++ myjs.js
@@ -1,1 +1,20 @@
-//a[a[a[a[a[]]]]]
+$(function(){
+    $(".ajaxForm").submit(function(e){
+        e.preventDefault();
+        var href = $(this).attr("action");
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: href,
+            data: $(this).serialize(),
+            success: function(response){
+                if(response.status == "success"){
+                    alert("Отправка совершена успешно!");
+                }
+                else{
+                    alert("Ошибка: " + response.message);
+                }
+            }
+        });
+    });
+});
