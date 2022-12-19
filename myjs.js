b7cb98ee4a6ac5ddd1adaca995ef612ffbda00f8
OlegGayvoronsky <113290639+OlegGayvoronsky@users.noreply.github.com>
2022-12-19 09:59:29 +0300

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -9,7 +9,7 @@ $(function(){
             data: $(this).serialize(),
             success: function(response){
                 if(response.status == "success"){
-                    alert("Отправка совершена успешно!");
+                    alert("Данные успешно отправлены!");
                 }
                 else{
                     alert("Ошибка: " + response.message);
@@ -18,3 +18,34 @@ $(function(){
         });
     });
 });
+$(document).on('ready', function() {
+    $('.worked_with_us_slider1').slick({
+      infinite: true,
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
+  });
+  
+  $(document).on('ready', function() {
+    $('.worked_with_us_slider2').slick({
+      infinite: true,
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+    });
+  });
+  
+  
+  $(document).on('ready', function() {
+    $('.rewiews_slider').slick({
+      infinite: true,
+      adaptiveHeight: true,
+      fade: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+  
+    });
+  })
